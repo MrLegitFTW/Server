3f2c3c6f9dd23ac286c05c89f581ddcbfdc0804a
sebas <sebastienmarc.harris@gmail.com>
2025-09-06 11:43:15 +0100

    scrript

--- setupScript.sh
+++ setupScript.sh
@@ -1,4 +1,5 @@
-npm run build -- --legacy-peer-deps
+npm install --legacy-peer-deps
+npm run build
 sudo cp -r dist/* /var/www/html/
 sudo chown -R root:root /var/www/html
 sudo chmod -R 755 /var/www/html
