4db78759f560d42325b2e21da58fa524de26cb81
sebas <Sebastienmarc.harris@gmail.com>
2025-09-10 18:17:23 +0200

    add stepper

--- routes.jsx
+++ routes.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/HomePage";
 import TestPage from "./pages/TestPage";
 import AboutWebsite from "./pages/WebsitePage.jsx";
 import AboutHelpPage from "./pages/HelpPage.jsx";
+import AITutorialPage from "./pages/AITutorialPage.jsx";
 
 export default function AppRouter() {
     return (
@@ -12,7 +13,7 @@ export default function AppRouter() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about/help" element={<AboutHelpPage/>} />
                 <Route path="/about/website" element={<AboutWebsite/>} />
-                <Route path="/tutorials/ai" element={<TestPage />} />
+                <Route path="/tutorials/ai" element={<AITutorialPage />} />
                 <Route path="/tutorials/jellyfin" element={<TestPage />} />
                 <Route path="/tutorials/notebook" element={<TestPage />} />
                 <Route path="/tutorials/nas" element={<TestPage />} />
