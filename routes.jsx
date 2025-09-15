b304ba7980c7efc25f043a683e4a7d636e916233
sebas <Sebastienmarc.harris@gmail.com>
2025-09-15 11:25:41 +0200

    Add skeleton tutorial pages

--- routes.jsx
+++ routes.jsx
@@ -4,6 +4,9 @@ import TestPage from "./pages/TestPage";
 import AboutWebsite from "./pages/WebsitePage.jsx";
 import AboutHelpPage from "./pages/HelpPage.jsx";
 import AITutorialPage from "./pages/AITutorialPage.jsx";
+import MediaTutorialPage from "./pages/MediaTutorialPage.jsx";
+import NASTutorialPage from "./pages/NASTutorialPage.jsx";
+import NotebookTutorialPage from "./pages/NotebookTutorialPage.jsx";
 
 export default function AppRouter() {
     return (
@@ -14,9 +17,9 @@ export default function AppRouter() {
                 <Route path="/about/help" element={<AboutHelpPage/>} />
                 <Route path="/about/website" element={<AboutWebsite/>} />
                 <Route path="/tutorials/ai" element={<AITutorialPage />} />
-                <Route path="/tutorials/jellyfin" element={<TestPage />} />
-                <Route path="/tutorials/notebook" element={<TestPage />} />
-                <Route path="/tutorials/nas" element={<TestPage />} />
+                <Route path="/tutorials/jellyfin" element={<MediaTutorialPage />} />
+                <Route path="/tutorials/notebook" element={<NotebookTutorialPage />} />
+                <Route path="/tutorials/nas" element={<NASTutorialPage />} />
             </Routes>
         </Router>
     );
